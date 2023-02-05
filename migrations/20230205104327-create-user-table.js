7d346543a4e08c387b78321227a948eecbd6a5ca
naoyuki42 <naoyuki.7042@outlook.jp>
2023-02-05 22:41:39 +0900

    add: migration file

---
+++ migrations/20230205104327-create-user-table.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable("users", {
+    id: {
+      type: "int",
+      primaryKey: true,
+      unsigned: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: "string",
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: "string",
+      notNull: true,
+    },
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable("user");
+};
+
+exports._meta = {
+  version: 1,
+};
