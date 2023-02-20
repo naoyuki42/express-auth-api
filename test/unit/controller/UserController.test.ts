fef330394e634e2b8ea83763db59f4bfc7c02639
naoyuki42 <naoyuki.7042@outlook.jp>
2023-02-20 22:02:00 +0900

    add: user controller test

--- test/unit/controller/UserController.test.ts
+++ test/unit/controller/UserController.test.ts
@@ -74,3 +74,106 @@ describe("userGetメソッド", () => {
     await expect(controller.userGet(request)).rejects.toThrow(expected);
   });
 });
+
+describe("userCreateメソッド", () => {
+  let mockContext: MockContext;
+  let controller: UserController;
+
+  beforeEach(() => {
+    mockContext = createMockContext();
+    const context = mockContext as unknown as Context;
+    controller = new UserController(context);
+  });
+
+  test("正常系", async () => {
+    // 前準備
+    const request = getMockReq({
+      body: {
+        userName: "naoyuki42",
+        password: "na0yuk1&42",
+      },
+    });
+    const hashedPassword = await hash("na0yuk1&42", 10);
+    const mockUser = {
+      id: 1,
+      name: "naoyuki42",
+      password: hashedPassword,
+      token: null,
+    };
+    jest.spyOn(mockContext.prisma.user, "create").mockResolvedValue(mockUser);
+    // 想定結果
+    const expected = {
+      userId: 1,
+    };
+    // 対象メソッドの戻り値が想定結果と同じであること
+    await expect(controller.userCreate(request)).resolves.toStrictEqual(
+      expected
+    );
+  });
+
+  test("異常系:DBからエラーがあった場合", async () => {
+    // 前準備
+    const request = getMockReq({
+      body: {
+        userName: "naoyuki42",
+        password: "na0yuk1&42",
+      },
+    });
+    jest
+      .spyOn(mockContext.prisma.user, "create")
+      .mockRejectedValue(new Error());
+    // 想定結果
+    const expected = new Error();
+    // 対象メソッドの戻り値が想定結果と同じであること
+    await expect(controller.userCreate(request)).rejects.toThrow(expected);
+  });
+});
+
+describe("userDeleteメソッド", () => {
+  let mockContext: MockContext;
+  let controller: UserController;
+
+  beforeEach(() => {
+    mockContext = createMockContext();
+    const context = mockContext as unknown as Context;
+    controller = new UserController(context);
+  });
+
+  test("正常系", async () => {
+    // 前準備
+    const request = getMockReq({
+      params: {
+        userId: "1",
+      },
+    });
+    const mockUser = {
+      id: 1,
+      name: "naoyuki42",
+      password: "na0yuk1&42",
+      token: null,
+    };
+    jest.spyOn(mockContext.prisma.user, "delete").mockResolvedValue(mockUser);
+    // 想定結果
+    const expected = {
+      userId: 1,
+    };
+    // 対象メソッドの戻り値が想定結果と同じであること
+    await expect(controller.userDelete(request)).resolves.not.toThrowError();
+  });
+
+  test("異常系:DBからエラーがあった場合", async () => {
+    // 前準備
+    const request = getMockReq({
+      params: {
+        userId: "1",
+      },
+    });
+    jest
+      .spyOn(mockContext.prisma.user, "delete")
+      .mockRejectedValue(new Error());
+    // 想定結果
+    const expected = new Error();
+    // 対象メソッドの戻り値が想定結果と同じであること
+    await expect(controller.userDelete(request)).rejects.toThrow(expected);
+  });
+});
